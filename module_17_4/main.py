badca12e67341acb49ee692b3b6c3cf2dae7e212
Edison <krylov2310@gmail.com>
2024-12-11 10:59:00 +0500

    Add files via upload

---
+++ module_17_4/main.py
@@ -0,0 +1,31 @@
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from app.routers import task, user
+
+app = FastAPI()
+info_ed = ('<h2>Домашнее задание по теме "Использование БД в маршрутизации. 1.1"<br>'
+           '<h3>Цель: научиться управлять записями в БД используя SQLAlchemy и маршрутизацию FastAPI.'
+           '<br>Задача "Маршрутизация пользователя":'
+           '<br>Студент Крылов Эдуард Васильевич'
+           '<br>Дата: 11.12.2024г.</h3>')
+
+
+# python -m uvicorn main:app
+# Get
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+@app.get("/info", response_class=HTMLResponse)
+async def info():
+    return info_ed
+
+
+app.include_router(task.router)
+app.include_router(user.router)
+# python -m uvicorn main:app
+# alembic revision --autogenerate -m "Initial migration"
+# python main.py migrate
+# python -m uvicorn main:app
+# uvicorn main:app --reload --port 8001
